8b8ac26523d44e49d85917363285c760947ac97a
Carla Lopez <carlalopez3108@gmail.com>
2026-01-21 16:03:23 +0100

    remove allocations for now

--- modules/su3.c
+++ modules/su3.c
@@ -17,34 +17,6 @@
 #include <string.h>
 #include <stdio.h>
 
-int alloc_su3_cdble(su3_cdble **su3)
-{
-    // confirm size of su3_cdble is 18 * sizeof(double)
-    _Static_assert(sizeof(su3_cdble) == 18 * sizeof(double),
-                   "su3_cdble layout assumption broken");
-    *su3 = malloc(sizeof **su3);
-
-    if (*su3 == NULL)
-    {
-        return -1;
-    }
-    return 0;
-}
-
-int alloc_su3_vector_cdble(su3_vector_cdble **su3_vec)
-{
-    // confirm size of su3_cdble is 18 * sizeof(double)
-    _Static_assert(sizeof(su3_vector_cdble) == 6 * sizeof(double),
-                   "su3_vector_cdble layout assumption broken");
-    *su3_vec = malloc(sizeof **su3_vec);
-
-    if (*su3_vec == NULL)
-    {
-        return -1;
-    }
-    return 0;
-}
-
 void unit_su3_cdble(su3_cdble *su3)
 {
 
--- modules/ufields.c
+++ modules/ufields.c
@@ -13,27 +13,11 @@
 
 
 #include "su3.h"
+#include "su3v.h"
 #include "global.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int alloc_ufield(su3_cdble **field, size_t size)
-{
-    // confirm size of su3_cdble is 18 * sizeof(double)
-    _Static_assert(sizeof(su3_cdble) == 18 * sizeof(double),
-               "su3_cdble layout assumption broken");
-    for (size_t i = 0; i < size; i++) {
-        field[i] = malloc(sizeof(su3_cdble));
-        if (field[i] == NULL) {
-            return -1;
-        }
-    }
-    if (*field == NULL) {
-        return -1;
-    }
-    return 0;
-}
 
 void usu3xusu3(su3_cdble *res, su3_cdble *u_field, su3_cdble *v_field, size_t size)
 {
@@ -49,4 +33,8 @@ void usu3xusu3vec(su3_vector_cdble *res_field, su3_cdble *u_field, su3_vector_cd
     }
 }
 
+void usu3xusu3v(su3_field_dble *res, su3_field_dble *u_field, su3_field_dble *v_field, size_t size)
+{
+    // TODO: implement this function
+}
 # endif // UFLDS_C
