ee1beab273e63540db6d20bad6af4040dec38f5a
Carla Lopez <carlalopez3108@gmail.com>
2026-02-09 16:58:42 +0100

    cpu and gpu rooflines

--- scripts/make.sh
+++ scripts/make.sh
@@ -6,10 +6,11 @@ export GCC="$(which clang)"
 
 cd ..
 
+export OMP_NUM_THREADS=1
 rm -rf build
 cmake -S . -B build \
-  -DCMAKE_C_COMPILER=/usr/bin/gcc \
+  -DCMAKE_C_COMPILER=$GCC \
   -DCMAKE_BUILD_TYPE=Debug -DENABLE_OPENMP=ON -DENABLE_AVX=ON
 cmake --build build -- -j8
 
-./build/main/time 10 0
+./build/main/time 500 1023
--- scripts/plot_roofline.py
+++ scripts/plot_roofline.py
@@ -1,28 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-labels = ["CPU", "AVX", "AVX2", "AVX512", "GPU FP64", "GPU FP32"]
+labels_cpu = ["CPU", "AVX", "AVX2", "AVX512", "16Cores"]
+labels_gpu = ["GPU FP64", "GPU FP32"]
 
 # Roofline parameters for phase II
-peak_performances = np.array([12, 12*2, 12*4, 12*8, 124.8, 7987.2])  # in GFlops/s
-memory_bandwidths = np.array([39, 39, 39, 39, 288, 288])  # in GB/s
+peak_performances_cpu = np.array([12, 12*2, 12*4, 12*8, 12*16])  # in GFlops/s
+memory_bandwidths_cpu = np.array([39, 39, 39, 39, 460.8])  # in GB/s
+
+peak_performances_gpu = np.array([124.8, 7987.2])  # in GFlops/s
+memory_bandwidths_gpu = np.array([288, 288])  # in GB/s
 
 # Operational intensities (FLOPs/Byte)
-ops_intensity = peak_performances / memory_bandwidths
-print(ops_intensity)
+print("CPU Ridge points:", peak_performances_cpu / memory_bandwidths_cpu)
+print("GPU Ridge points:", peak_performances_gpu / memory_bandwidths_gpu)
 
 # Create the log-log roofline plot
 plt.figure(figsize=(6, 4))
 
-# Plot roofline
-for i in range(len(peak_performances)):
+# Plot roofline CPU
+for i in range(len(peak_performances_cpu)):
     x = np.linspace(0.001,  2**10, 100000)
 
-    y = np.minimum(x*memory_bandwidths[i], peak_performances[i])
-    plt.plot(x, y, label=labels[i])
+    y = np.minimum(x*memory_bandwidths_cpu[i], peak_performances_cpu[i])
+    plt.plot(x, y, label=labels_cpu[i])
+
+# Plot roofline GPU
+for i in range(len(peak_performances_gpu)):
+    x = np.linspace(0.001,  2**10, 100000)
+
+    y = np.minimum(x*memory_bandwidths_gpu[i], peak_performances_gpu[i])
+    plt.plot(x, y, label=labels_gpu[i])
 
 # add su3matmat
-plt.vlines(0.4583, 0.001, 100, linestyles='dashdot', colors="black", label="SU(3) matmul")
+# plt.vlines(0.4583, 0.001, 100, linestyles='dashdot', colors="black", label="SU(3) matmul")
 
 # Add labels and legend
 plt.xlabel('Operational Intensity (FLOPs/Byte)')
