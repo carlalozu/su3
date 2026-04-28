0e81a310fd5e8a926aababa1c84bfeea56e46337
Carla Lopez <calopez@student.ethz.ch>
2026-04-28 16:17:03 +0200

    LQCD_DEVICE based on library

--- include/ufields.h
+++ include/ufields.h
@@ -13,27 +13,25 @@
 
 #include "su3.h"
 #include "su3v.h"
-#ifndef __CUDACC__
 #include "global.h"
-#endif
 
-#ifdef __CUDACC__
-  #define LQCD_DEVICE    __device__
-  #define LQCD_OMP_BEGIN
-  #define LQCD_OMP_END
-#elif defined(KOKKOS_CORE_HPP)
+#if defined(KOKKOS_CORE_HPP)
   #define LQCD_DEVICE    KOKKOS_INLINE_FUNCTION
   #define LQCD_OMP_BEGIN
   #define LQCD_OMP_END
+#elif defined(__CUDACC__)
+  #define LQCD_DEVICE    __device__ static inline
+  #define LQCD_OMP_BEGIN
+  #define LQCD_OMP_END
 #else
-  #define LQCD_DEVICE
+  #define LQCD_DEVICE static inline
   #define LQCD_OMP_BEGIN _Pragma("omp declare target")
   #define LQCD_OMP_END   _Pragma("omp end declare target")
 #endif
 
 // SoA operations
 LQCD_OMP_BEGIN
-LQCD_DEVICE static inline void fsu3matxsu3vec(su3_vec_dble *res,const su3_mat_field *u,const su3_vec_field *v,const size_t i)
+LQCD_DEVICE void fsu3matxsu3vec(su3_vec_dble *res,const su3_mat_field *u,const su3_vec_field *v,const size_t i)
 {
     res->c1re = u->c1.c1re[i] * v->c1re[i] - u->c1.c1im[i] * v->c1im[i] +
                 u->c1.c2re[i] * v->c2re[i] - u->c1.c2im[i] * v->c2im[i] +
@@ -64,7 +62,7 @@ LQCD_OMP_END
  * r.c3=(u^dagger*s).c3
  */
 LQCD_OMP_BEGIN
-LQCD_DEVICE static inline void fsu3matdagxsu3vec(su3_vec_dble *r, const su3_mat_field *u, const su3_vec_field *s, const size_t i)
+LQCD_DEVICE void fsu3matdagxsu3vec(su3_vec_dble *r, const su3_mat_field *u, const su3_vec_field *s, const size_t i)
 {
     r->c1re = u->c1.c1re[i] * s->c1re[i] + u->c1.c1im[i] * s->c1im[i] +
               u->c2.c1re[i] * s->c2re[i] + u->c2.c1im[i] * s->c2im[i] +
@@ -88,7 +86,7 @@ LQCD_DEVICE static inline void fsu3matdagxsu3vec(su3_vec_dble *r, const su3_mat_
 LQCD_OMP_END
 
 LQCD_OMP_BEGIN
-LQCD_DEVICE static inline double su3matdxsu3matd_retrace(const su3_mat_dble *u, const su3_mat_dble *v)
+LQCD_DEVICE double su3matdxsu3matd_retrace(const su3_mat_dble *u, const su3_mat_dble *v)
 {
     double tr_1 = u->c1.c1re * v->c1.c1re - u->c1.c1im * v->c1.c1im 
                 + u->c1.c2re * v->c2.c1re - u->c1.c2im * v->c2.c1im 
@@ -104,7 +102,7 @@ LQCD_DEVICE static inline double su3matdxsu3matd_retrace(const su3_mat_dble *u, 
 LQCD_OMP_END
 
 LQCD_OMP_BEGIN
-LQCD_DEVICE static inline void fsu3matxsu3mat(su3_mat_dble *res, const su3_mat_field *u, const su3_mat_field *v, const size_t i)
+LQCD_DEVICE void fsu3matxsu3mat(su3_mat_dble *res, const su3_mat_field *u, const su3_mat_field *v, const size_t i)
 {
     res->c1.c1re = u->c1.c1re[i] * v->c1.c1re[i] - u->c1.c1im[i] * v->c1.c1im[i] +
                    u->c1.c2re[i] * v->c1.c2re[i] - u->c1.c2im[i] * v->c1.c2im[i] +
@@ -169,7 +167,7 @@ LQCD_OMP_END
  * Computes w=u^dag*v^dag assuming that w is different from u and v.
  */
 LQCD_OMP_BEGIN
-LQCD_DEVICE static inline void fsu3matdagxsu3matdag(su3_mat_dble *w, const su3_mat_field *u, const su3_mat_field *v, const size_t i)
+LQCD_DEVICE void fsu3matdagxsu3matdag(su3_mat_dble *w, const su3_mat_field *u, const su3_mat_field *v, const size_t i)
 {
     w->c1.c1re = u->c1.c1re[i] * v->c1.c1re[i] + u->c1.c1im[i] * -v->c1.c1im[i] +
                  u->c2.c1re[i] * v->c1.c2re[i] + u->c2.c1im[i] * -v->c1.c2im[i] +
